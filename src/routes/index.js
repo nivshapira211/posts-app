e19b27c3fae60b03f1e64262c0c5a95688f5e8c3
nivshapira211@gmail.com <niv@Nivs-MacBook-Air.local>
2026-01-17 16:52:06 +0200

    added jwt routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import * as postsController from "../controllers/postsController.js";
 import * as commentsController from "../controllers/commentsController.js";
+import * as authController from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => res.send("Hello, This is Niv&Ofek posts app!"));
+
+// Auth routes
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
+router.post("/auth/logout", authController.logout);
+router.post("/auth/refresh", authController.refresh);
 
 // Post routes
 router.post("/post", postsController.createPost);
